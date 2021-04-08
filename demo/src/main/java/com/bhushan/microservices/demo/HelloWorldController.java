bd0b18ca5a018f1632c62731af58d1ce0ca6cf77
bhushan_kumar <bhushan_kumar@192.168.1.79>
2021-04-08 02:40:24 -0500

    initial commit - microservices practice

---
+++ demo/src/main/java/com/bhushan/microservices/demo/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.bhushan.microservices.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController{
+	
+
+
+
+	@GetMapping("/home")
+	public String HelloWorldString() {
+		
+		return "Hello Bhushan ! ";
+	}
+	
+}
